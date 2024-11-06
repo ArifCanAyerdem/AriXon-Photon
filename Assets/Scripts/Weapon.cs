9e998c0c17c3c721fff72fe73c16a6d928c62ab5
Arif Can AYERDEM <arifcan_2000@hotmail.com>
2024-11-06 21:41:39 +0300

    +reload + reload animation +ammo UI

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Weapon : MonoBehaviour
 {
@@ -15,20 +16,66 @@ public float fireRate;
 public GameObject hitVFX;
 private float nextFire;
 
+[Header("Ammo")]
+
+public int mag=5;
+public int ammo=30;
+public int magAmmo=30;
+
+[Header("UI")]
+
+public TextMeshProUGUI magText;
+public TextMeshProUGUI ammoText;
+
+[Header("Animation")]
+public Animation animation;
+public AnimationClip reload;
+
+
+ void Start()
+{
+    magText.text=mag.ToString();
+ammoText.text=ammo+"/"+magAmmo;
+}
 void Update()
 {
     
-if(nextFire>0)
+if(nextFire>0) 
 nextFire -=Time.deltaTime;
 
 
-    if (Input.GetButton("Fire1") && nextFire<=0)
+    if (Input.GetButton("Fire1") && nextFire<=0 && ammo>0 && animation.isPlaying==false)
     {
         nextFire = 1/fireRate;
+
+        ammo--;
+
+        magText.text=mag.ToString();
+ammoText.text=ammo+"/"+magAmmo;
         Fire();
+    }
+    if(Input.GetKeyDown(KeyCode.R))
+    {
+        Reload();
     }
 }
 
+
+
+void Reload(){
+
+animation.Play( reload.name);
+
+if (mag>0){
+
+    mag--;
+    ammo=magAmmo;
+}
+
+magText.text=mag.ToString();
+ammoText.text=ammo+"/"+magAmmo;
+
+}
 
 void Fire(){
 
