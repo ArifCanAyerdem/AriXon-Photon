053263ccae14b9bc53e2eb849e9e92e7cd76153d
Arif Can AYERDEM <arifcan_2000@hotmail.com>
2024-11-10 21:53:50 +0300

    +Health Bar image

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun; 
 using TMPro; 
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int health;
 public bool isLocalPlayer;
 
+public RectTransform healthBar;
+private float originalHealthBarSize;
+
 
 
     [Header("UI")]
     public TextMeshProUGUI healthText;
+
+
+
+    void Start()
+    {
+        originalHealthBarSize = healthBar.sizeDelta.x;
+    }
+
+    void Update()
+    {
+        //healthBar.sizeDelta = new Vector2(originalHealthBarSize*health/100f,healthBar.sizeDelta.y);
+    }
     
     [PunRPC]
     public void TakeDamage(int _damage)
     {
         health -= _damage;
+
+        healthBar.sizeDelta = new Vector2(originalHealthBarSize*health/100f,healthBar.sizeDelta.y);
 
         healthText.text = health.ToString();
 
